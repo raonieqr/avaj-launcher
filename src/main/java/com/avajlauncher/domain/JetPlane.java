64e07242dc7b3cbccdc31ee9882794b8832a35a6
Raoni Silva <raoniestevan@gmail.com>
2024-10-07 20:41:26 -0300

    feat: implementation JetPlane

---
+++ src/main/java/com/avajlauncher/domain/JetPlane.java
@@ -0,0 +1,9 @@
+package main.java.com.avajlauncher.domain;
+
+public class JetPlane extends Aircraft{
+
+    public JetPlane(Long p_id, String p_name, Coordinates p_coordinate) {
+        super();
+    }
+    public void updateConditions() {}
+}
