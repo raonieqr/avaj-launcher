fd4be7df4f4e817be5aca1047cea2bd96a318f55
Raoni Silva <raoniestevan@gmail.com>
2024-10-07 20:38:57 -0300

    feat: implementation Aircraft

---
+++ src/main/java/com/avajlauncher/domain/Aircraft.java
@@ -0,0 +1,9 @@
+package main.java.com.avajlauncher.domain;
+
+public class Aircraft {
+    protected Long id;
+    protected String name;
+    protected Coordinates coordinates;
+
+    protected Aircraft(long p_id, String p_name, Coordinates p_coordinate) {}
+}
