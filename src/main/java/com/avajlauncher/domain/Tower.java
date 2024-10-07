897e8910385b5864d4da6c2f6671e074fb5cab2e
Raoni Silva <raoniestevan@gmail.com>
2024-10-07 20:34:58 -0300

    feat: implementation tower

---
+++ src/main/java/com/avajlauncher/domain/Tower.java
@@ -0,0 +1,20 @@
+package main.java.com.avajlauncher.domain;
+
+import main.java.com.avajlauncher.contracts.Flyable;
+import java.util.List;
+
+public class Tower {
+    private List<Flyable> observers;
+
+    public void register(Flyable p_flyable) {
+
+    }
+
+    public void unregister(Flyable p_flyable) {
+
+    }
+
+    protected void conditionChanged() {
+
+    }
+}
