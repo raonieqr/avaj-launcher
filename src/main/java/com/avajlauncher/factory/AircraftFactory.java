b0a87af4e7a000bf76b177aead8df00549232eba
Raoni Silva <raoniestevan@gmail.com>
2024-11-03 09:07:22 -0300

    feat: build aircraft

--- src/main/java/com/avajlauncher/factory/AircraftFactory.java
+++ src/main/java/com/avajlauncher/factory/AircraftFactory.java
@@ -1,25 +1,43 @@
 package com.avajlauncher.factory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.avajlauncher.contracts.Flyable;
 import com.avajlauncher.domain.*;
 
 public class AircraftFactory {
-    private Long id = 0L;
+  private Long id = 0L;
 
-    private Long generateId() {
-        return this.id++;
-    }
+  private Long generateId() {
+    return this.id++;
+  }
 
-    public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates) {
-        switch (p_type.toLowerCase()) {
-            case "baloon":
-                return new Baloon(generateId(), p_name, p_coordinates);
-            case "helicopter":
-                return new Helicopter(generateId(), p_name, p_coordinates);
-            case "jetplane":
-                return new JetPlane(generateId(), p_name, p_coordinates);
-            default:
-                throw new IllegalArgumentException("Unknown aircraft type: " + p_type);
-        }
+  public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates) {
+    switch (p_type.toLowerCase()) {
+      case "baloon":
+        return new Baloon(generateId(), p_name, p_coordinates);
+      case "helicopter":
+        return new Helicopter(generateId(), p_name, p_coordinates);
+      case "jetplane":
+        return new JetPlane(generateId(), p_name, p_coordinates);
+      default:
+        throw new IllegalArgumentException("Unknown aircraft type: " + p_type);
     }
+  }
+
+  public List<Flyable> buildAircraftFleet(List<String[]> aircraftData) {
+    List<Flyable> aircraftList = new ArrayList<>();
+
+    aircraftData.forEach(line -> {
+      for (String type : line) {
+
+        Flyable aircraft = AircraftFactory.newAircraft();
+
+        aircraftList.add(aircraft);
+      }
+    });
+
+    return aircraftList;
+  }
 }
